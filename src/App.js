b7fb918a33a867eb3f413bc0ead0dbd478d1ed25
Manir Uddin <manir22sust@gmail.com>
2023-01-23 21:28:58 +0100

    todos app using react-redux and data fetching with axios

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Todos } from "./components/Todos";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React redux</h1>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { todosReducer } from "./services/reducers/todosReducer";
+
+export const store = createStore(todosReducer, applyMiddleware(thunk));
